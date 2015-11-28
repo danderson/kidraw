752493524ca13eaab4c832a29f71af6c1e0a0098
David Anderson <dave@natulte.net>
2015-11-27 17:50:20 -0800

    Reimplement the main kidraw library, which just ties all the other things together.

---
+++ kidraw/__init__.py
@@ -0,0 +1,19 @@
+from kidraw import schematic, footprint, ipc
+from kidraw.ipc import reference as fpref
+
+__all__
+
+class Library(object):
+    def __init__(self, name):
+        self._name = name
+        self.devices = []
+
+    def save(self):
+        sch = [x.schematic.sch() for x in self.devices]
+        doc = [x.schematic.doc() for x in self.devices]
+        footprints = [(fn, str(f)) for fn, f in d.footprints.items() for d in self.devices]
+
+class Device(object):
+    def __init__(self, schematic=None, footprints=[]):
+        self.schematic = schematic
+        self.footprints = footprints
